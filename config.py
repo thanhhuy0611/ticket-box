7d45bc5c4e16122b723c4840f232d5438a5a9c7e
= <=>
2019-11-24 16:35:40 +0700

    fix

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class Config(object):
   DEBUG=True
-  SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_POSTGRES')
+  SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
   SECRET_KEY='supersecret'
   FLASK_ADMIN_SWATCH = 'cerulean'
   SQLALCHEMY_TRACK_MODIFICATIONS = False
